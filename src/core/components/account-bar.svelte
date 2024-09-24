e0fdd9e0f8ffb15bc97ce1e83042c574a92e466e
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-09-24 18:41:24 +0200

    fix

--- src/core/components/account-bar.svelte
+++ src/core/components/account-bar.svelte
@@ -15,22 +15,25 @@
  let accountsVisible = false;
  let selectedAccount = accountsArray[0];
  let items;
+ let dropdown;
 
  onMount(() => {
-  selectedAccount = selectAccount(1);
+  selectAccount(1);
  });
 
  function toggleAccounts() {
-  console.log('ACCOUNTS VISIBLE', accountsVisible);
-  console.log('CLASS', items.classList);
-  if (accountsVisible) items.classList.remove('open');
-  else items.classList.add('open');
-  accountsVisible = accountsVisible ? false : true;
+  accountsVisible = !accountsVisible;
  }
 
  function selectAccount(id) {
   selectedAccount = accountsArray.find(a => a.id === id);
-  console.log(id);
+  accountsVisible = false;
+ }
+
+ function handleClickOutside(event) {
+  if (dropdown && !dropdown.contains(event.target)) {
+   accountsVisible = false;
+  }
  }
 </script>
 
@@ -38,9 +41,6 @@
  .dropdown {
   display: flex;
   align-items: center;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  overflow: hidden;
   padding: 10px;
   font-weight: bold;
   border-bottom: 1px solid #555;
@@ -50,6 +50,9 @@
  }
 
  .dropdown .text {
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
   flex-grow: 1;
  }
 
@@ -60,6 +63,7 @@
 
  .items {
   display: none;
+  flex-direction: column;
  }
 
  .items.open {
@@ -77,16 +81,20 @@
  }
 
  .items .item:hover {
-  background-color: yellow;
+  background-color: #222;
  }
 </style>
 
-<div class="dropdown" role="button" tabindex="0" on:click={toggleAccounts}>
+<div class="dropdown" role="button" tabindex="0" on:click={toggleAccounts} bind:this={dropdown}>
  <div class="text">{selectedAccount.title}</div>
  <div><img src="img/down.svg"></div>
 </div>
-<div class="items" bind:this={items}>
+<div class="items" class:open={accountsVisible} bind:this={items}>
  {#each accountsArray as a (a.id)}
   <div class="item" role="button" tabindex="0" on:click={() => selectAccount(a.id)}>{a.title}</div>
  {/each}
 </div>
+
+{#if accountsVisible}
+ <div on:click|self={handleClickOutside} style="position: fixed; top: 0; left: 0; width: 100%; height: 100%;"></div>
+{/if}
