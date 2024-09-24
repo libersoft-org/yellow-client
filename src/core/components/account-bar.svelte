94857cb985e1e8f9a68422ae4b24fe0465153967
Liberland Software Foundation <info@libersoft.org>
2024-09-24 18:44:14 +0200

    Add dropdown accounts

--- src/core/components/account-bar.svelte
+++ src/core/components/account-bar.svelte
@@ -1,20 +1,12 @@
 <script>
- import { onMount } from 'svelte';
+ import { onMount, onDestroy } from 'svelte';
  let accountsArray = [
-  {
-   id: 1,
-   title: 'user@user.com (AMTP)'
-  }, {
-   id: 2,
-   title: 'jinejuser@jinejuser.com (AMTP)'
-  }, {
-   id: 3,
-   title: '1a2b3c4d5e6f7a8b9c0d1f2a3b4c5d6e7f8a9b0c1d2e3f (DMTP)'
-  },
+  { id: 1, title: 'user@user.com (AMTP)' },
+  { id: 2, title: 'jinejuser@jinejuser.com (AMTP)' },
+  { id: 3, title: '1a2b3c4d5e6f7a8b9c0d1f2a3b4c5d6e7f8a9b0c1d2e3f (DMTP)' },
  ];
  let accountsVisible = false;
  let selectedAccount = accountsArray[0];
- let items;
  let dropdown;
 
  onMount(() => {
@@ -22,19 +14,27 @@
  });
 
  function toggleAccounts() {
-  accountsVisible = !accountsVisible;
+   accountsVisible = !accountsVisible;
+   if (accountsVisible) document.addEventListener('click', handleClickOutside);
+   else document.removeEventListener('click', handleClickOutside);
  }
 
  function selectAccount(id) {
   selectedAccount = accountsArray.find(a => a.id === id);
   accountsVisible = false;
+  document.removeEventListener('click', handleClickOutside);
  }
 
  function handleClickOutside(event) {
   if (dropdown && !dropdown.contains(event.target)) {
    accountsVisible = false;
+   document.removeEventListener('click', handleClickOutside);
   }
  }
+
+ onDestroy(() => {
+  document.removeEventListener('click', handleClickOutside);
+ });
 </script>
 
 <style>
@@ -47,6 +47,7 @@
   background-color: #222;
   color: #fff;
   cursor: pointer;
+  position: relative;
  }
 
  .dropdown .text {
@@ -64,6 +65,12 @@
  .items {
   display: none;
   flex-direction: column;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  background-color: #333;
+  z-index: 1000;
+  width: 100%;
  }
 
  .items.open {
@@ -75,10 +82,9 @@
   text-overflow: ellipsis;
   overflow: hidden;
   padding: 10px;
-  background-color: #333;
   color: #fff;
   cursor: pointer;
- }
+}
 
  .items .item:hover {
   background-color: #222;
@@ -87,14 +93,12 @@
 
 <div class="dropdown" role="button" tabindex="0" on:click={toggleAccounts} bind:this={dropdown}>
  <div class="text">{selectedAccount.title}</div>
- <div><img src="img/down.svg"></div>
+ <div><img src="img/down.svg" /></div>
+ {#if accountsVisible}
+  <div class="items open">
+   {#each accountsArray as a (a.id)}
+    <div class="item" role="button" tabindex="0" on:click={() => selectAccount(a.id)}>{a.title}</div>
+   {/each}
+  </div>
+ {/if}
 </div>
-<div class="items" class:open={accountsVisible} bind:this={items}>
- {#each accountsArray as a (a.id)}
-  <div class="item" role="button" tabindex="0" on:click={() => selectAccount(a.id)}>{a.title}</div>
- {/each}
-</div>
-
-{#if accountsVisible}
- <div on:click|self={handleClickOutside} style="position: fixed; top: 0; left: 0; width: 100%; height: 100%;"></div>
-{/if}
