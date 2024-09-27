fea6af94a64bf6b20d13f72d3c76d1cb6db80bbd
Liberland Software Foundation <info@libersoft.org>
2024-09-27 20:14:30 +0200

    Add welcome wizard step

--- src/core/wizards/welcome-step1.svelte
+++ src/core/wizards/welcome-step1.svelte
@@ -1,1 +1,40 @@
-<div>This is a step 1 content... test test test test test long text</div>
+<style>
+ .title {
+  font-size: 25px;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 5px;
+ }
+
+ .description {
+  font-size: 13px;
+  margin-bottom: 20px;
+ }
+
+ @keyframes bounce {
+  0%, 20%, 50%, 80%, 100% {
+   transform: translateY(0);
+  }
+  40% {
+   transform: translateY(-30px);
+  }
+  60% {
+   transform: translateY(-15px);
+  }
+ }
+
+ .logo {
+  display: flex;
+  justify-content: center;
+  animation: bounce 2s infinite;
+ }
+
+ .logo img {
+  width: 100px;
+  height: 100px;
+ }
+</style>
+
+<div class="title">Welcome to Yellow</div>
+<div class="description">The best decentralized communication app</div>
+<div class="logo"><img src="img/logo.svg" alt="logo" /></div>
