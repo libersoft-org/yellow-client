3d5a2497ed0a7f992c1ba02fabcfddaf168b19f2
Liberland Software Foundation <info@libersoft.org>
2024-09-27 14:21:01 +0200

    accounts

---
+++ src/core/components/accounts-action-button.svelte
@@ -0,0 +1,30 @@
+<script>
+ export let img;
+ export let title;
+ export let id;
+ export let click;
+ 
+ function keyAction(id) {
+  if (event.key === 'Enter' || event.key === ' ') {
+   event.preventDefault();
+   click(id);
+  }
+ }
+</script>
+
+<style>
+ .item {
+  padding: 10px;
+  background-color: red;
+  cursor: pointer;
+ }
+
+ .item img {
+  width: 20px;
+  height: 20px;
+ }
+</style>
+
+<div class="item" role="button" tabindex="0" on:click={() => click({id})} on:keydown={() => keyAction({id})}>
+ <img src={img} alt="{title}" />
+</div>
--- src/core/pages/accounts.svelte
+++ src/core/pages/accounts.svelte
@@ -1,3 +1,16 @@
+<script>
+ import { accounts } from '../core.js';
+ import ActionButton from '../components/accounts-action-button.svelte';
+
+ function clickEdit(id) {
+  console.log('EDIT', id);
+ }
+
+ function clickDel(id) {
+  console.log('DEL', id);
+ }
+</script>
+
 <style>
  .accounts {
   display: flex;
@@ -6,6 +19,50 @@
   background: url('/img/background.webp') repeat;
   background-size: 400px;
  }
+
+ .accounts table {
+  border-spacing: 0;
+  border: 1px solid #000;
+  border-radius: 10px;
+  overflow: hidden;
+ }
+
+ .accounts table thead tr {
+  background-color: #222;
+  color: #fff;
+  text-align: left;
+ }
+
+ .accounts table tbody tr:nth-child(even) {
+  background-color: #ffdd1130;
+ }
+
+ .accounts table tbody tr:nth-child(odd) {
+  background-color: #ffdd1150;
+ }
+
+ .accounts table tbody tr:hover {
+  background-color: #fd1;
+ }
 </style>
 
-<div class="accounts">Accounts</div>
+<div class="accounts">
+ <div class="button">Add new account</div>
+ <table>
+  <thead>
+   <th>Account</th>
+   <th>Action</th>
+  </thead>
+  <tbody>
+   <tr>
+    {#each $accounts as a (a.id)}
+     <td>{a.id}</td>
+     <td>
+      <ActionButton img="img/edit.svg" title="Edit" on:click={() => clickEdit(a.id)} />
+      <ActionButton img="img/del.svg" title="Delete" on:click={() => clickDel(a.id)} />
+     </td>
+    {/each}
+   </tr>
+  </tbody>
+ </table>
+</div>
