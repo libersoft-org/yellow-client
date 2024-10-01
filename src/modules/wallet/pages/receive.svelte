1fd5bb84326f3d3f9f9a1d1a55e2efacd77047ae
Liberland Software Foundation <info@libersoft.org>
2024-10-01 19:03:44 +0200

    Add receive form - start

--- src/modules/wallet/pages/receive.svelte
+++ src/modules/wallet/pages/receive.svelte
@@ -1,9 +1,24 @@
 <script>
+ import { get } from 'svelte/store';
+ import QRCode from 'qrcode';
+ import { selectedWallet } from '../wallet.js';
+ let qrCodeData = '';
+ 
+ $: if ($selectedWallet) generateQRCode();
 
+ function generateQRCode() {
+  QRCode.toDataURL(get(selectedWallet).address, { width: 150 })
+  .then(url => qrCodeData = url)
+  .catch(err => console.error(err));
+ };
 </script>
 
 <style>
-
 </style>
 
-<div>RECEIVE</div>
+{#if $selectedWallet}
+ <div>{$selectedWallet.address}</div>
+ <div class="qr"><img src={qrCodeData} alt={$selectedWallet.address} /></div>
+{:else}
+ <div>No wallet selected</div>
+{/if}
