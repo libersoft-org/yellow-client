571ce00dc90e9e6dfbbe851b39453a1e05174d54
Your Name <you@example.com>
2024-10-07 23:03:57 +0200

    print

--- src/modules/wallet/modals/new-wallet.svelte
+++ src/modules/wallet/modals/new-wallet.svelte
@@ -39,6 +39,56 @@
  function print() {
   // TODO: print preview and print
   console.log('PRINT');
+  const newWindow = window.open('', '_blank');
+  newWindow.document.write(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>QR Code</title>
+      <style>
+        .qr {
+          display: flex;
+          justify-content: center;
+          margin-top: 20px;
+        }
+        .cell {
+          padding: 5px;
+          border: 1px solid #000;
+        }
+        table {
+          margin: 20px auto;
+          border-collapse: collapse;
+        }
+      </style>
+    </head>
+    <body>
+      <div>Use the following QR code to transfer your wallet seed phrase to your other device, never show it to anyone else!</div>
+      <div class="qr"><img src="${qrCodeData}" alt="Seed phrase" /></div>
+      <div>Write down or print these 24 words, also known as seed phrase. It will serve as a backup of your wallet. Cut it into 2 parts (12 + 12 words) and hide it in 2 different places, where you don't have your devices. Never show it to anyone else!</div>
+      <table>
+        <tr>
+          <td>
+            ${phrase.split(' ').slice(0, 6).map((word, index) => `<div class="cell">${index + 1}. ${word}</div>`).join('')}
+          </td>
+          <td>
+            ${phrase.split(' ').slice(6, 12).map((word, index) => `<div class="cell">${index + 7}. ${word}</div>`).join('')}
+          </td>
+          <td>
+            ${phrase.split(' ').slice(12, 18).map((word, index) => `<div class="cell">${index + 13}. ${word}</div>`).join('')}
+          </td>
+          <td>
+            ${phrase.split(' ').slice(18, 24).map((word, index) => `<div class="cell">${index + 19}. ${word}</div>`).join('')}
+          </td>
+        </tr>
+      </table>
+    </body>
+    </html>
+  `);
+  newWindow.document.close();
+  newWindow.print();
+
  }
 </script>
 
