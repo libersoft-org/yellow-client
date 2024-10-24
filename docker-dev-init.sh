56f8d6a5b18b7fc5a02883aac69513513485aaa5
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-10-24 02:30:30 +0200

    svelte 4

--- docker-dev-init.sh
+++ docker-dev-init.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-
-rm -rf ./node_modules
-mkdir node_modules
-ls -thrlsa ./node_modules/
+set -e
 
 npm i
-
 rm -rf ./node_modules/yellow-server-common || true
 ln -s /app/lib/yellow-server-common ./node_modules/
 
