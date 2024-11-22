18575b36c52401e83e1f16d8d195293baa425055
Liberland Software Foundation <info@libersoft.org>
2024-11-22 12:32:09 +0100

    Fix account status icon to left side

--- src/core/components/account-bar-item.svelte
+++ src/core/components/account-bar-item.svelte
@@ -25,12 +25,16 @@
  .item:hover {
   background-color: #222;
  }
+
+ .item .title {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+ }
 </style>
 
 <div class="item" role="button" tabindex="0" on:click={() => clickSelectAccount($a.id)} on:keydown={event => keySelectAccount($a.id, event)}>
- {$a.settings?.title} - {$a.credentials?.address}
- <AccountStatusIcon {a} />
-
+ <div class="title"><AccountStatusIcon {a} />{$a.settings?.title} - {$a.credentials?.address}</div>
  <small>
   <ul>
    <li>enabled: {$a.enabled}</li>
