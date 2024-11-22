1a3e3ccff558bacfb3f9494ba79a60011d91f392
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-11-22 17:29:58 +0100

    barvicky

--- src/core/components/account-status-icon.svelte
+++ src/core/components/account-status-icon.svelte
@@ -1,5 +1,17 @@
 <script>
  export let a;
+
+ $: status_class = (
+  $a.session_status === 'Logged in.' ? 'status-logged-in' : $a.status === 'Connecting...'
+   ? 'status-connecting' : $a.status === 'Connected, logging in...'
+    ? 'status-logging-in' : $a.status === 'Connected.'
+     ? 'status-connected' : $a.status === 'Retrying...'
+      ? 'status-retrying' : $a.status === 'Login failed.'
+       ? 'status-login-failed' : $a.status === 'Disabled.'
+        ? 'status-disabled' : $a.status === 'Enabled.'
+         ? 'status-enabled' : 'status-default'
+ );
+
 </script>
 
 <style>
@@ -9,42 +21,51 @@
   border-radius: 50%;
   display: inline-block;
  }
+
  .status-logged-in {
   background-color: #080;
   border: 1px solid #040;
  }
- .status-logging-in {
-  background-color: #fd3;
-  border: 1px solid #b90;
- }
+
  .status-login-failed {
   background-color: #f00;
   border: 1px solid #800;
  }
+
  .status-retrying {
-  background-color: #fa0;
   background-color: #b60;
+  border: 1px solid #820;
  }
+
  .status-connecting {
   background-color: #fd3;
-  border: 1px solid #b90;
+  border: 1px solid #690000;
  }
+
  .status-connected {
   background-color: #fd3;
   border: 1px solid #b90;
  }
+
+ .status-logging-in {
+  background-color: #ffffff;
+  border: 1px solid #198700;
+ }
+
  .status-enabled {
-  background-color: #fd3;
+  background-color: #002eb8;
   border: 1px solid #b90;
  }
+
  .status-default {
   background-color: #00f;
   border: 1px solid #008;
  }
+
  .status-disabled {
   background-color: #888;
   border: 1px solid #444;
  }
 </style>
 
-<span class="status-icon {$a.session_status === 'Logged in.' ? 'status-logged-in' : $a.status === 'Connecting...' ? 'status-connecting' : $a.status === 'Connected, logging in...' ? 'status-logging-in' : $a.status === 'Connected.' ? 'status-connected' : $a.status === 'Retrying...' ? 'status-retrying' : $a.status === 'Login failed.' ? 'status-login-failed' : $a.status === 'Disabled.' ? 'status-disabled' : $a.status === 'Enabled.' ? 'status-enabled' : 'status-default'}"></span>
+<span class="status-icon {status_class}"></span>
