4abf8c77817933195de2811fd537dc714c802ed2
Liberland Software Foundation <info@libersoft.org>
2024-12-08 16:02:16 +0100

    Add background to contacts

--- src/modules/org.libersoft.contacts/pages/contacts-content.svelte
+++ src/modules/org.libersoft.contacts/pages/contacts-content.svelte
@@ -1,1 +1,10 @@
-<div>Contact list - content page - not yet implemented</div>
+<style>
+ .content {
+  height: 100vh;
+  background: url('/img/background.webp') repeat;
+  background-size: 400px;
+  overflow: hidden;
+ }
+</style>
+
+<div class="content">Contact list - content page - not yet implemented</div>
