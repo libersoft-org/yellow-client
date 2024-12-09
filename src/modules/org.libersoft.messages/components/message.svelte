8000b65d99cdf7b1b6f5fb8f5dcc0883055d827b
Liberland Software Foundation <info@libersoft.org>
2024-12-09 19:10:21 +0100

    Add upload / download progress bar in messages

--- src/modules/org.libersoft.messages/components/message.svelte
+++ src/modules/org.libersoft.messages/components/message.svelte
@@ -7,6 +7,7 @@
  import ContextMenuItem from '../../../core/components/context-menu-item.svelte';
  import Audio from './audio.svelte';
  //import Video from './video.svelte';
+ //import FileTransfer from './filetransfer.svelte';
  //import Reply from './message-reply.svelte';
  export let message;
  export let container_element;
@@ -253,6 +254,7 @@
  <!-- <Reply name="Someone" text="Some text" /> -->
  <Audio file="modules/org.libersoft.messages/audio/message.mp3" />
  <!--<Video file="https://file-examples.com/storage/fe3abb0cc967520c59b97f1/2017/04/file_example_MP4_1920_18MG.mp4" />-->
+ <!--<FileTransfer file="text.mp4" uploaded="10485760000" total="20000000000" />-->
  <div class="text">{@html processMessage(message.message)}</div>
  <div class="bottomline">
   <div class="time">{new Date(message.created /*.replace(' ', 'T') + 'Z'*/).toLocaleString()}</div>
