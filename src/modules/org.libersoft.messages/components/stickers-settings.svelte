fdf06225f655ea571f06fbbbe3a0598a56646a13
Liberland Software Foundation <info@libersoft.org>
2024-12-25 21:58:09 +0100

    Fix CSS

---
+++ src/modules/org.libersoft.messages/components/stickers-settings.svelte
@@ -0,0 +1,32 @@
+<script>
+ import Button from '../../../core/components/button.svelte';
+ export let stickerServer;
+
+ function clickUpdate() {}
+</script>
+
+<style>
+ .group {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+ }
+
+ .group .label {
+  font-weight: bold;
+ }
+
+ .group input {
+  width: 10px;
+  padding: 10px;
+  border: 1px solid #000;
+  flex-grow: 1;
+  border-radius: 10px;
+ }
+</style>
+
+<div class="group">
+ <div class="label">Sticker server:</div>
+ <input type="text" bind:value={stickerServer} />
+</div>
+<Button text="Save & update" on:click={clickUpdate} />
--- src/modules/org.libersoft.messages/components/stickers.svelte
+++ src/modules/org.libersoft.messages/components/stickers.svelte
@@ -6,33 +6,30 @@
  import Button from '../../../core/components/button.svelte';
  import Tabs from './stickers-tabs.svelte';
  import Item from './stickers-tabs-item.svelte';
+ import TabSettings from './stickers-settings.svelte';
+ import TabFavourites from './stickers-favourites.svelte';
+ import TabServer from './stickers-server.svelte';
  const library = localStorageSharedStore('stickers', {});
  let stickerServer = 'https://stickers.libersoft.org';
  let filter;
- let activeSource = 'server';
+ let activeTab = 'server';
+ const tabs = {
+  favourites: TabFavourites,
+  settings: TabSettings,
+  server: TabServer,
+ };
 
  onMount(async () => {
   if ($library[stickerServer] === undefined) await updateStickerLibrary(library, stickerServer);
   filter.focus(); // TODO - not working, because it loads the element before it's shown in context menu
  });
 
- function clickSourceServer() {
-  activeSource = 'server';
-  console.log('Clicked From server');
- }
-
- function clickSourceFavourites() {
-  activeSource = 'fav';
-  console.log('Clicked My favourites');
+ function setTab(e, name) {
+  activeTab = name;
  }
 
  function clickUpdate() {
   updateStickerLibrary(library, stickerServer);
- }
-
- function clickSettings() {
-  activeSource = 'settings';
-  console.log('Clicked on Settings');
  }
 
  function clickSearch() {
@@ -53,42 +50,19 @@
   flex-direction: column;
   gap: 20px;
  }
-
- .group {
-  display: flex;
-  align-items: center;
-  gap: 10px;
- }
-
- .group .label {
-  font-weight: bold;
- }
-
- .group input {
-  width: 10px;
-  padding: 10px;
-  border: 1px solid #000;
-  flex-grow: 1;
-  border-radius: 10px;
- }
 </style>
 
 <div class="stickers">
  <Tabs>
-  <Item img="modules/org.libersoft.messages/img/favourite.svg" active={activeSource === 'fav'} onClick={clickSourceFavourites} />
-  <Item img="modules/org.libersoft.messages/img/server.svg" active={activeSource === 'server'} onClick={clickSourceServer} />
+  <Item img="modules/org.libersoft.messages/img/favourite.svg" active={activeTab === 'favourites'} onClick={e => setTab(e, 'favourites')} />
+  <Item img="modules/org.libersoft.messages/img/server.svg" active={activeTab === 'server'} onClick={e => setTab(e, 'server')} />
   <Item img="modules/org.libersoft.messages/img/update.svg" onClick={clickUpdate} />
-  <Item img="img/settings.svg" active={activeSource === 'settings'} onClick={clickSettings} />
+  <Item img="img/settings.svg" active={activeTab === 'settings'} onClick={e => setTab(e, 'settings')} />
  </Tabs>
+ <svelte:component this={tabs[activeTab]} />
  <div class="group">
-  <div class="label">Sticker server:</div>
-  <input type="text" bind:value={stickerServer} />
-  <Button on:click={clickUpdate}>Update</Button>
- </div>
- <div class="group">
-  <div class="label">Filter:</div>
   <input type="text" bind:this={filter} />
-  <Button on:click={clickSearch}>Search</Button>
+  <Button text="Search" on:click={clickSearch} />
  </div>
  {#if library}
   <div class="set">
