bf84d55878a6f622a7fb3924aa599763839a8f42
Liberland Software Foundation <info@libersoft.org>
2024-12-26 21:33:43 +0100

    Fix sidebar item click

--- src/modules/org.libersoft.dating/components/sidebar-item.svelte
+++ src/modules/org.libersoft.dating/components/sidebar-item.svelte
@@ -1,14 +1,8 @@
 <script>
+ import BaseButton from '../../../core/components/base-button.svelte';
  export let img = null;
  export let label = '';
  export let onClick;
-
- function handleKeydown(event) {
-  if (event.key === 'Enter' || event.key === ' ') {
-   event.preventDefault();
-   dispatch('click');
-  }
- }
 </script>
 
 <style>
@@ -20,7 +14,6 @@
   padding: 10px;
   background-color: #fffcf0;
   border-bottom: 1px solid #dd9;
-  cursor: pointer;
  }
 
  .item .image {
@@ -29,9 +22,11 @@
  }
 </style>
 
-<div class="item" role="button" tabindex="0" on:click={onClick} on:keydown={handleKeydown}>
- {#if img}
-  <div class="image"><img src="modules/org.libersoft.dating/img/{img}" alt={label} /></div>
- {/if}
- <div>{label}</div>
-</div>
+<BaseButton {onClick}>
+ <div class="item">
+  {#if img}
+   <div class="image"><img src="modules/org.libersoft.dating/img/{img}" alt={label} /></div>
+  {/if}
+  <div>{label}</div>
+ </div>
+</BaseButton>
