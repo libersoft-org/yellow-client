9a3859d1dac341328b9011b7a47650dbe41d63e4
Liberland Software Foundation <info@libersoft.org>
2024-12-28 22:55:53 +0100

    Giphy

--- src/modules/org.libersoft.messages/components/gifset.svelte
+++ src/modules/org.libersoft.messages/components/gifset.svelte
@@ -1,23 +1,52 @@
 <script>
- import { onMount } from 'svelte';
+ import { onMount, getContext } from 'svelte';
+ import BaseButton from '../../../core/components/base-button.svelte';
  import Button from '../../../core/components/button.svelte';
  import InputText from '../../../core/components/input-text.svelte';
- let searchTerm = '';
+ import { localStorageSharedStore } from '../../../lib/svelte-shared-store.ts';
+ const MessageBar = getContext('MessageBar');
+ const store = localStorageSharedStore('giphy-api-key');
+ let apiKey;
  let gifs = [];
+ let query = '';
  let loading = false;
 
+ onMount(() => {
+  apiKey = $store;
+ });
+
+ function saveAPIKey() {
+  store.set(apiKey);
+ }
+
+ function keySaveAPIKey(e) {
+  if (e.detail.key === 'Enter') saveAPIKey();
+ }
+
  async function getGifs() {
-  if (!searchTerm) return;
+  if (!query) return;
   loading = true;
   try {
-   let response = await fetch('https://tenor.googleapis.com/v2/search?q=' + encodeURIComponent(searchTerm) + 'limit=10');
-   let data = await response.json();
-   gifs = data.results.map(gif => gif.media_formats.gif.url);
-  } catch (error) {
-   console.error('Error while fetching GIFs:', error);
+   const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=12&offset=0&rating=r&lang=en&bundle=messaging_non_clips`);
+   console.log(response);
+   if (!response.ok) throw new Error(`HTTP Error: ${response.status}`);
+   const data = await response.json();
+   gifs = data.data.map(gif => gif.images.fixed_height.url);
+  } catch (err) {
+   console.error('Error while loading GIFs from server:', err);
   } finally {
    loading = false;
   }
+ }
+
+ function keyGetGifs(e) {
+  if (e.detail.key === 'Enter') getGifs();
+ }
+
+ function sendGIF(url) {
+  console.log('Sending GIF:', url);
+  MessageBar.sendMessage('<img src="' + url + '" alt="GIF" />');
+  MessageBar.setBarFocus();
  }
 </script>
 
@@ -29,7 +58,7 @@
   padding: 10px;
  }
 
- .controls {
+ .group {
   display: flex;
   gap: 10px;
  }
@@ -48,18 +77,24 @@
 </style>
 
 <div class="gifset">
- <div class="controls">
-  <InputText placeholder="Search GIFs..." bind:value={searchTerm} on:keydown={e => e.key === 'Enter' && fetchGifs()} />
+ <div class="group">
+  <InputText placeholder="Your Giphy API key" bind:value={apiKey} on:keydown={keySaveAPIKey} />
+  <Button text="Save" onClick={saveAPIKey} />
+ </div>
+ <div class="group">
+  <InputText placeholder="Search GIFs" bind:value={query} on:keydown={keyGetGifs} />
   <Button text="Search" onClick={getGifs} />
  </div>
  {#if loading}
   <div>Loading...</div>
- {:else if gifs.length === 0 && searchTerm}
+ {:else if gifs.length === 0 && query}
   <div>No GIFs found.</div>
  {:else}
   <div class="container">
    {#each gifs as gif}
-    <img src={gif} alt="GIF" />
+    <BaseButton onClick={() => sendGIF(gif)}>
+     <img src={gif} alt="GIF" />
+    </BaseButton>
    {/each}
   </div>
  {/if}
