cc47f8d2b1f3004be3cf48b1d8a7a4060ad05088
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-12-30 19:58:09 +0100

    ..

--- src/modules/org.libersoft.messages/components/intersector.svelte
+++ src/modules/org.libersoft.messages/components/intersector.svelte
@@ -1,11 +1,13 @@
 <script>
+ import { writable } from 'svelte/store';
+
  let { items } = $props();
 
  let observer;
  let container;
  let itemsEls = [];
  let itemsById = {};
- let visibility = {};
+ let visibility = writable({});
 
  $effect(() => {
   itemsById = {};
@@ -32,7 +34,10 @@
     let n = Number(entry.target.dataset.id);
     console.log('n:', n, 'entry.target.dataset.id:', entry.target.dataset.id, 'intersection:', entry.isIntersecting);
     //itemsById[n]._intersecting = entry.isIntersecting;
-    visibility[n] = entry.isIntersecting;
+    visibility.update(x => {
+     x[n] = entry.isIntersecting;
+     return x;
+    });
     //visibility[entry.target.dataset.id] = entry.isIntersecting;
    });
   }, 60);
@@ -47,19 +52,20 @@
  }
 </style>
 
-{JSON.stringify(visibility)}
+{JSON.stringify($visibility)}
 <div class="intersector" bind:this={container}>
  {#each items as item, i (item.id)}
   <div class="item" data-id={item.id} bind:this={itemsEls[i]}>
+   <!--
    {JSON.stringify(typeof item.id)}
    {JSON.stringify(item.id)}
-   {JSON.stringify(visibility[item.id])}
+   {JSON.stringify($visibility[item.id])}
+-->
 
-   {#if visibility[item.id]}
-    yyy
+   {#if $visibility[item.id]}
     <slot {item} intersecting={true} />
    {:else}
-    nnn
+    <div style="height: 100px; background-color: #ddd; border-radius: 10px;"></div>
    {/if}
   </div>
  {/each}
