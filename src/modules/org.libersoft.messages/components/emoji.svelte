d9f372f00e7fd7d857c3d225d9c4f3e81615373a
Your Name <koom@c1>
2025-01-14 16:58:46 +0100

    ..

--- src/modules/org.libersoft.messages/components/emoji.svelte
+++ src/modules/org.libersoft.messages/components/emoji.svelte
@@ -1,9 +1,12 @@
 <script>
  import { debug } from '../../../core/core.js';
- import { rgi, emoji_render } from '../emojis.js';
+ import { rgi, emoji_render, render_emojis_as_static } from '../emojis.js';
  export let codepoints;
  export let size = 40;
  export let is_single = false;
+
+ let url;
+ $: url = "https://fonts.gstatic.com/s/e/notoemoji/latest/" + rgi(codepoints) + ($render_emojis_as_static ? "/emoji.svg" : "/512.webp");
 
 </script>
 
@@ -17,4 +20,4 @@
 </style>
 
 {#if $debug}{JSON.stringify(codepoints)}{/if}
-<img style="{!is_single && 'padding: 0 2px;'} min-width: {size}px; min-height: {size}px; max-width: {size}px; max-height: {size}px;" src="https://fonts.gstatic.com/s/e/notoemoji/latest/{rgi(codepoints)}/emoji.svg" loading="lazy" alt={emoji_render(codepoints)} />
+<img style="{!is_single && 'padding: 0 2px;'} min-width: {size}px; min-height: {size}px; max-width: {size}px; max-height: {size}px;" src="{url}" loading="lazy" alt={emoji_render(codepoints)} />
