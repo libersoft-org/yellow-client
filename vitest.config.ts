1c7a05d432451fc3a3df353f5c4a15eb88897519
Your Name <koom@c1>
2025-01-17 16:48:25 +0100

    tests

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    include: ['src/**/*.test.js'],
+  },
+  resolve: process.env.VITEST
+		? {
+				conditions: ['browser']
+			}
+		: undefined
+});
+});
