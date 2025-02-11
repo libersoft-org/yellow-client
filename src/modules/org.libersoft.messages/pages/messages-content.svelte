e0278e2c538a73d6b0d2a68d167cc6c20241fafb
LiberSoft <info@libersoft.org>
2025-02-11 16:19:28 +0100

    Module offline

--- src/modules/org.libersoft.messages/pages/messages-content.svelte
+++ src/modules/org.libersoft.messages/pages/messages-content.svelte
@@ -1,27 +1,13 @@
 <script>
- import { identifier, selectedConversation, online } from '../messages.js';
+ import { selectedConversation } from '../messages.js';
  import Welcome from './welcome.svelte';
  import Conversation from '../components/conversation.svelte';
  import Gallery from '../components/gallery.svelte';
 </script>
 
-<style>
- .offline {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  font-size: 20px;
- }
-</style>
-
-{#if $online}
- {#if $selectedConversation === null}
-  <Welcome />
- {:else}
-  <Conversation />
- {/if}
- <Gallery />
+{#if $selectedConversation === null}
+ <Welcome />
 {:else}
- <div class="offline">Offline</div>
+ <Conversation />
 {/if}
+<Gallery />
