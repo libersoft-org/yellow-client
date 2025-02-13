a450b9a83e774b7c3404863a07941ac3ec778bae
Your Name <koom@c1>
2025-02-13 18:09:12 +0100

    lock

--- docker-dev-init.sh
+++ docker-dev-init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-npm i
-#bun i --frozen-lockfile
+bun i --frozen-lockfile
+
 rm -rf ./node_modules/yellow-server-common || true
 ln -s /app/lib/yellow-server-common ./node_modules/
 
