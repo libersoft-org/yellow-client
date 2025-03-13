20d1260dab9123eef9299f3b09e2590563cab3b7
LiberSoft <info@libersoft.org>
2025-03-13 18:22:33 +0100

    WIP - Add notifications

---
+++ src/core/components/notification.svelte
@@ -0,0 +1,10 @@
+<style>
+ .notification {
+  padding: 10px;
+  border: 1px solid #000;
+  border-radius: 10px;
+  background-color: red;
+ }
+</style>
+
+<div class="notification">Notification</div>
---
+++ src/routes/notifications/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+ import '../../app.css';
+ import { writable } from 'svelte/store';
+ import Notification from '../../core/components/notification.svelte';
+ //let notifications = writable([]);
+</script>
+
+<style>
+ .notifications {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  background-color: yellow;
+ }
+</style>
+
+<div class="notifications">
+ <Notification />
+ <Notification />
+ <Notification />
+ <Notification />
+</div>
