a2e5210b428e2c36a418eb379aa869e83387b834
LiberSoft <info@libersoft.org>
2025-04-01 16:29:48 +0200

    Settings menu

--- src/core/modals/settings.svelte
+++ src/core/modals/settings.svelte
@@ -1,25 +1,28 @@
 <script>
  import { get } from 'svelte/store';
+ import MenuItem from '../components/menu-item.svelte';
  import Table from '../components/table.svelte';
  import Tbody from '../components/table-tbody.svelte';
  import Tr from '../components/table-tbody-tr.svelte';
  import Td from '../components/table-tbody-td.svelte';
  import Switch from '../components/switch.svelte';
- import Tabs from '../components/tabs.svelte';
- import TabsItem from '../components/tabs-item.svelte';
  import SettingsAppearance from '../components/settings-appearance.svelte';
  import SettingsNotifications from '../components/settings-notifications.svelte';
  import { tick } from 'svelte';
-
- // Tab selection
  let activeTab = 'appearance';
+ let menuItemProps = {
+  bgColor: '#fff',
+  textColor: '#000',
+  hoverColor: '#eee',
+  borderColor: '#ddd',
+  borderRadius: '10px',
+ };
 </script>
 
 <style>
  .settings-container {
   display: flex;
   flex-direction: column;
-  gap: 20px;
  }
 
  .tab-content {
@@ -35,11 +38,8 @@
 </style>
 
 <div class="settings-container">
- <Tabs>
-  <TabsItem label="Appearance" active={activeTab === 'appearance'} onClick={() => (activeTab = 'appearance')} />
-  <TabsItem label="Notifications" active={activeTab === 'notifications'} onClick={() => (activeTab = 'notifications')} />
- </Tabs>
-
+ <MenuItem img="img/appearance-black.svg" title="Appearance" bgColor={menuItemProps.bgColor} textColor={menuItemProps.textColor} hoverColor={menuItemProps.hoverColor} borderColor={menuItemProps.borderColor} borderRadius={menuItemProps.borderRadius} onClick={() => (activeTab = 'appearance')} />
+ <MenuItem img="img/notification-black.svg" title="Notifications" bgColor={menuItemProps.bgColor} textColor={menuItemProps.textColor} hoverColor={menuItemProps.hoverColor} borderColor={menuItemProps.borderColor} borderRadius={menuItemProps.borderRadius} onClick={() => (activeTab = 'notifications')} />
  <div class="tab-content">
   {#if activeTab === 'appearance'}
    <SettingsAppearance />
