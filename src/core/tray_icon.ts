936975f7ef0eb1b853ace915b00d008e628faa8a
Your Name <koom@c1>
2025-04-09 16:49:54 +0200

    ..

--- src/core/tray_icon.ts
+++ src/core/tray_icon.ts
@@ -1,36 +1,41 @@
-import { handleIconState, moveWindow, Position } from '@tauri-apps/plugin-positioner';
-import { TrayIcon } from '@tauri-apps/api/tray';
-import { IS_TAURI, IS_TAURI_MOBILE, log } from './tauri.ts';
-import { resolveResource } from '@tauri-apps/api/path';
-import { defaultWindowIcon } from '@tauri-apps/api/app';
+import {
+  handleIconState,
+  moveWindow,
+  Position,
+} from "@tauri-apps/plugin-positioner";
+import { TrayIcon } from "@tauri-apps/api/tray";
+import { IS_TAURI, IS_TAURI_MOBILE, log } from "./tauri.ts";
+import { resolveResource } from "@tauri-apps/api/path";
+import { defaultWindowIcon } from "@tauri-apps/api/app";
 
 export async function createTrayIcon() {
- if (IS_TAURI && !IS_TAURI_MOBILE) {
-  try {
-   // Get the path to the icon file
-   const iconPath = await defaultWindowIcon();
-   //resolveResource('icons/icon.png');
+  if (IS_TAURI && !IS_TAURI_MOBILE) {
+    try {
+      // Get the path to the icon file
+      const iconPath = await defaultWindowIcon();
+      //resolveResource('icons/icon.png');
 
-   const action = async event => {
-    log.debug(`TrayIcon event: ${event.type}`);
-    // add the handle in the action to update the state
-    await handleIconState(event);
+      const action = async (event) => {
+        log.debug(`TrayIcon event: ${event.type}`);
+        // add the handle in the action to update the state
+        await handleIconState(event);
 
-    if (event.type === 'Click') {
-     // note this option requires enabling the `tray-icon`
-     //   feature in the Cargo.toml
-     await moveWindow(Position.TrayLeft);
+        if (event.type === "Click") {
+          // note this option requires enabling the `tray-icon`
+          //   feature in the Cargo.toml
+          await moveWindow(Position.TrayLeft);
+        }
+      };
+      const options = {
+        id: "main",
+        icon: iconPath,
+        action,
+        tooltip: "awesome tray tooltip",
+      };
+      const tray = await TrayIcon.new(options);
+      log.debug("TrayIcon created:", tray);
+    } catch (error) {
+      log.debug("Error creating tray icon:", error);
     }
-   };
-   const options = {
-    id: 'main',
-    icon: iconPath,
-    action,
-   };
-   const tray = await TrayIcon.new(options);
-   log.debug('TrayIcon created:', tray);
-  } catch (error) {
-   log.debug('Error creating tray icon:', error);
   }
- }
 }
