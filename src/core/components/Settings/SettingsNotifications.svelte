3113cd99c6d6ad0cf99d3010e619180b09befa05
LiberSoft <info@libersoft.org>
2025-05-02 17:56:31 +0200

    Some warning in Notification settings

--- src/core/components/Settings/SettingsNotifications.svelte
+++ src/core/components/Settings/SettingsNotifications.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+ import Alert from '../Alert/Alert.svelte';
  import Table from '../Table/Table.svelte';
  import TableTBodyTr from '../Table/TableTBodyTr.svelte';
  import TableTBody from '../Table/TableTBody.svelte';
@@ -21,98 +22,104 @@
  }
 </script>
 
-<Table expand={true}>
- <TableTBody>
-  <SettingsNotificationsBasic />
+<style>
+ .settings-notifications {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+ }
+</style>
 
-  {#if CUSTOM_NOTIFICATIONS}
-   {#if $customNotificationsOn}
-    <TableTBodyTr>
-     <TableTBodyTd>
-      <div class="bold">Animation:</div>
-     </TableTBodyTd>
-     <TableTBodyTd center={true}>
-      <Select bind:value={$animationName}>
-       <SelectOption value="none" text="None" />
-       <SelectOption value="zoom" text="Zoom" />
-       <SelectOption value="opacity" text="Opacity" />
-      </Select>
-     </TableTBodyTd>
-    </TableTBodyTr>
-
-    <TableTBodyTr>
-     <TableTBodyTd>
-      <div class="bold">Animation duration:</div>
-     </TableTBodyTd>
-     <TableTBodyTd center={true}>
-      <Input type="number" bind:value={$animationDuration} min={0} max={1000} step={10} />ms
-     </TableTBodyTd>
-    </TableTBodyTr>
-
-    <TableTBodyTr>
-     <TableTBodyTd>
-      <div class="bold">Maximum number of lines in title:</div>
-     </TableTBodyTd>
-     <TableTBodyTd center={true}>
-      <Input type="number" bind:value={$titleMaxLines} min={1} max={3} />
-     </TableTBodyTd>
-    </TableTBodyTr>
-
-    <TableTBodyTr>
-     <TableTBodyTd>
-      <div class="bold">Maximum number of lines in description:</div>
-     </TableTBodyTd>
-     <TableTBodyTd center={true}>
-      <Input type="number" bind:value={$bodyMaxLines} min={1} max={5} />
-     </TableTBodyTd>
-    </TableTBodyTr>
-
-    <TableTBodyTr>
-     <TableTBodyTd>
-      <div class="bold">Background color:</div>
-     </TableTBodyTd>
-     <TableTBodyTd center={true}>
-      <input type="color" bind:value={$bgColor} />
-      {$bgColor}
-     </TableTBodyTd>
-    </TableTBodyTr>
-    <TableTBodyTr>
-     <TableTBodyTd>
-      <div class="bold">Background color on mouse over:</div>
-     </TableTBodyTd>
-     <TableTBodyTd center={true}>
-      <input type="color" bind:value={$bgColorHover} />
-      {$bgColorHover}
-     </TableTBodyTd>
-    </TableTBodyTr>
-    <TableTBodyTr>
-     <TableTBodyTd>
-      <div class="bold">Border color:</div>
-     </TableTBodyTd>
-     <TableTBodyTd center={true}>
-      <input type="color" bind:value={$borderColor} />
-      {$borderColor}
-     </TableTBodyTd>
-    </TableTBodyTr>
-    <TableTBodyTr>
-     <TableTBodyTd>
-      <div class="bold">Title color:</div>
-     </TableTBodyTd>
-     <TableTBodyTd center={true}>
-      <input type="color" bind:value={$titleColor} />
-      {$titleColor}
-     </TableTBodyTd>
-    </TableTBodyTr>
-    <TableTBodyTr>
-     <TableTBodyTd>
-      <div class="bold">Description color:</div>
-     </TableTBodyTd>
-     <TableTBodyTd center={true}>
-      <input type="color" bind:value={$descColor} />
-      {$descColor}
-     </TableTBodyTd>
-    </TableTBodyTr>
+<div class="settings-notifications">
+ <Alert type="error" message="Some warning" />
+ <Table expand={true}>
+  <TableTBody>
+   <SettingsNotificationsBasic />
+   {#if CUSTOM_NOTIFICATIONS}
+    {#if $customNotificationsOn}
+     <TableTBodyTr>
+      <TableTBodyTd>
+       <div class="bold">Animation:</div>
+      </TableTBodyTd>
+      <TableTBodyTd center={true}>
+       <Select bind:value={$animationName}>
+        <SelectOption value="none" text="None" />
+        <SelectOption value="zoom" text="Zoom" />
+        <SelectOption value="opacity" text="Opacity" />
+       </Select>
+      </TableTBodyTd>
+     </TableTBodyTr>
+     <TableTBodyTr>
+      <TableTBodyTd>
+       <div class="bold">Animation duration:</div>
+      </TableTBodyTd>
+      <TableTBodyTd center={true}>
+       <Input type="number" bind:value={$animationDuration} min={0} max={1000} step={10} />ms
+      </TableTBodyTd>
+     </TableTBodyTr>
+     <TableTBodyTr>
+      <TableTBodyTd>
+       <div class="bold">Maximum number of lines in title:</div>
+      </TableTBodyTd>
+      <TableTBodyTd center={true}>
+       <Input type="number" bind:value={$titleMaxLines} min={1} max={3} />
+      </TableTBodyTd>
+     </TableTBodyTr>
+     <TableTBodyTr>
+      <TableTBodyTd>
+       <div class="bold">Maximum number of lines in description:</div>
+      </TableTBodyTd>
+      <TableTBodyTd center={true}>
+       <Input type="number" bind:value={$bodyMaxLines} min={1} max={5} />
+      </TableTBodyTd>
+     </TableTBodyTr>
+     <TableTBodyTr>
+      <TableTBodyTd>
+       <div class="bold">Background color:</div>
+      </TableTBodyTd>
+      <TableTBodyTd center={true}>
+       <input type="color" bind:value={$bgColor} />
+       {$bgColor}
+      </TableTBodyTd>
+     </TableTBodyTr>
+     <TableTBodyTr>
+      <TableTBodyTd>
+       <div class="bold">Background color on mouse over:</div>
+      </TableTBodyTd>
+      <TableTBodyTd center={true}>
+       <input type="color" bind:value={$bgColorHover} />
+       {$bgColorHover}
+      </TableTBodyTd>
+     </TableTBodyTr>
+     <TableTBodyTr>
+      <TableTBodyTd>
+       <div class="bold">Border color:</div>
+      </TableTBodyTd>
+      <TableTBodyTd center={true}>
+       <input type="color" bind:value={$borderColor} />
+       {$borderColor}
+      </TableTBodyTd>
+     </TableTBodyTr>
+     <TableTBodyTr>
+      <TableTBodyTd>
+       <div class="bold">Title color:</div>
+      </TableTBodyTd>
+      <TableTBodyTd center={true}>
+       <input type="color" bind:value={$titleColor} />
+       {$titleColor}
+      </TableTBodyTd>
+     </TableTBodyTr>
+     <TableTBodyTr>
+      <TableTBodyTd>
+       <div class="bold">Description color:</div>
+      </TableTBodyTd>
+      <TableTBodyTd center={true}>
+       <input type="color" bind:value={$descColor} />
+       {$descColor}
+      </TableTBodyTd>
+     </TableTBodyTr>
+    {/if}
    {/if}
-  {/if}
- </TableTBody>
-</Table>
+  </TableTBody>
+ </Table>
+</div>
