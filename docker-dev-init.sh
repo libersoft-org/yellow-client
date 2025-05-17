3344d0e134ea5dc151448e57ab274f08f546331c
Jindrich Kolman <kolman.jindrich@gmail.com>
2025-05-17 16:05:44 +0200

    ci..

--- docker-dev-init.sh
+++ docker-dev-init.sh
@@ -3,6 +3,7 @@ set -e
 echo "init"
 ~/.bun/bin/bun i --frozen-lockfile
 
-rm -rf ./node_modules/yellow-server-common || true
-ln -s /app/lib/yellow-server-common ./node_modules/
-
+if [ -n "$CI" ]; then
+ rm -rf ./node_modules/yellow-server-common || true
+ ln -s /app/lib/yellow-server-common ./node_modules/
+fi
