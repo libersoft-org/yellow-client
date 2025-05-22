71825a008dc81726b6625b954bd6b5025d228a8e
Your Name <koom@c1>
2025-05-22 17:57:28 +0200

    debug..

--- src/modules/org.libersoft.iframes/pages/sidebar.svelte
+++ src/modules/org.libersoft.iframes/pages/sidebar.svelte
@@ -1,5 +1,10 @@
 <script>
+ import core from '@/core/core.js';
+ import Button from '@/core/components/Button/Button.svelte';
+
  let url = 'https://koo5.github.io/';
 </script>
+
+<Button text="hideSidebarMobile" onClick={ () => core.hideSidebarMobile.set(true) } />
 
 <iframe sandbox="allow-scripts" src={url} title="sidebar" style="border:none; height: 100%; x-scroll: none; width: 100%; overflow: hidden;"> </iframe>
