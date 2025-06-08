d6dfe44ce52fe10ecf6d8360cd0b4cd6339abf70
Jindrich Kolman <kolman.jindrich@gmail.com>
2025-06-08 16:22:38 +0200

    vitest fix?

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,26 @@
-import { defineConfig, mergeConfig } from 'vitest/config';
-import viteConfig from './vite.config.js';
+import { defineConfig } from 'vitest/config';
+import path from 'node:path';
 
-export default mergeConfig(
-	viteConfig,
-	defineConfig({
-		test: {
-			environment: 'jsdom',
-			include: ['**/unit/*.{test,spec}.?(c|m)[jt]s?(x)'],
-			exclude: ['**/e2e/**'],
-			setupFiles: ['./vitest.shims.js'],
-			globals: true,
+export default defineConfig({
+	test: {
+		environment: 'jsdom',
+		include: ['**/unit/*.{test,spec}.?(c|m)[jt]s?(x)'],
+		exclude: ['**/e2e/**'],
+		setupFiles: ['./vitest.shims.js'],
+		globals: true,
+	},
+	resolve: {
+		conditions: ['browser'],
+		alias: {
+			'@/org.libersoft.messages': path.resolve(__dirname, 'src/modules/org.libersoft.messages'),
+			'@/org.libersoft.dating': path.resolve(__dirname, 'src/modules/org.libersoft.dating'),
+			'@': path.resolve(__dirname, 'src'),
+			'@/bridge/core-bridge': process.env.TAURI_SERVICE === 'true' ? path.resolve(__dirname, 'src/modules/org.libersoft.messages/core-bridge-mobile.ts') : path.resolve(__dirname, 'src/modules/org.libersoft.messages/core-bridge-builtin.ts'),
 		},
-	})
-);
+	},
+	define: {
+		__BUILD_DATE__: null,
+		__COMMIT_HASH__: JSON.stringify('test-commit-hash'),
+		__BRANCH__: JSON.stringify('test-branch'),
+	},
+});
