890dd7a29aecf0c70f0bd9936739381ee5cb3cc5
Jindrich Kolman <kolman.jindrich@gmail.com>
2025-06-08 15:21:02 +0200

    Add conditional core-bridge alias
    Add a new alias "@/bridge/core-bridge" that resolves to different modules based on the TAURI_SERVICE environment variable. This change allows the project to use either the mobile or built-in version of the core bridge depending on the environment.

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,23 +2,24 @@ import { defineConfig } from 'vitest/config';
 import path from 'node:path';
 
 export default defineConfig({
- test: {
-  environment: 'jsdom',
-  include: ['**/unit/*.{test,spec}.?(c|m)[jt]s?(x)'],
-  exclude: ['**/e2e/**'],
-  setupFiles: ['./vitest.shims.js'],
-  globals: true,
- },
- resolve: {
-  conditions: ['browser'],
-  alias: {
-   '@/org.libersoft.messages': path.resolve(__dirname, 'src/modules/org.libersoft.messages'),
-   '@/org.libersoft.dating': path.resolve(__dirname, 'src/modules/org.libersoft.dating'),
-   '@': path.resolve(__dirname, 'src'),
-  },
- },
- define: {
-  __BUILD_DATE__: null,
-  __COMMIT_HASH__: JSON.stringify('test-commit-hash'),
- },
+	test: {
+		environment: 'jsdom',
+		include: ['**/unit/*.{test,spec}.?(c|m)[jt]s?(x)'],
+		exclude: ['**/e2e/**'],
+		setupFiles: ['./vitest.shims.js'],
+		globals: true,
+	},
+	resolve: {
+		conditions: ['browser'],
+		alias: {
+			'@/org.libersoft.messages': path.resolve(__dirname, 'src/modules/org.libersoft.messages'),
+			'@/org.libersoft.dating': path.resolve(__dirname, 'src/modules/org.libersoft.dating'),
+			'@': path.resolve(__dirname, 'src'),
+			'@/bridge/core-bridge': process.env.TAURI_SERVICE === 'true' ? path.resolve(__dirname, 'src/modules/org.libersoft.messages/core-bridge-mobile.ts') : path.resolve(__dirname, 'src/modules/org.libersoft.messages/core-bridge-builtin.ts'),
+		},
+	},
+	define: {
+		__BUILD_DATE__: null,
+		__COMMIT_HASH__: JSON.stringify('test-commit-hash'),
+	},
 });
