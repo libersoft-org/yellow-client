1a50a99f05b46002a582b9383572682ee8646b94
LiberSoft <info@libersoft.org>
2025-06-18 19:01:05 +0200

    Fix clickable

--- src/core/components/Clickable/Clickable.svelte
+++ src/core/components/Clickable/Clickable.svelte
@@ -8,6 +8,15 @@
 		onMousedown?: MouseEventHandler<HTMLDivElement>;
 	}
 	let { children, onClick, onRightClick, onMousedown, ...restProps }: Props = $props();
+
+	function handleKeyDown(e: KeyboardEvent) {
+		if (e.key === 'Enter') (e.currentTarget as HTMLElement).click();
+		if (e.key === ' ') e.preventDefault();
+	}
+
+	function handleKeyUp(e: KeyboardEvent) {
+		if (e.key === ' ') (e.currentTarget as HTMLElement).click();
+	}
 </script>
 
 <style>
@@ -21,9 +30,10 @@
 
 	.clickable:focus-visible {
 		outline: 2px solid var(--primary-harder-background);
+		border-radius: 10px;
 	}
 </style>
 
-<div class="clickable" role="button" tabindex="0" onclick={onClick} onmousedown={onMousedown} oncontextmenu={onRightClick} {...restProps}>
+<div class="clickable" role="button" tabindex="0" onclick={onClick} onmousedown={onMousedown} oncontextmenu={onRightClick} onkeydown={handleKeyDown} onkeyup={handleKeyUp} {...restProps}>
 	{@render children?.()}
 </div>
