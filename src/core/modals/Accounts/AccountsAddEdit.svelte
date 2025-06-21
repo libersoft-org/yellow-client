681c9f47ff00fe30096a2ee6950421514c950fa5
LiberSoft <info@libersoft.org>
2025-06-21 21:34:41 +0200

    Fix add a new account

--- src/core/modals/Accounts/AccountsAddEdit.svelte
+++ src/core/modals/Accounts/AccountsAddEdit.svelte
@@ -7,9 +7,9 @@
 		show?: boolean;
 	}
 	let { params, show = $bindable(false) }: Props = $props();
-	let snippet_top: Snippet;
-	let snippet_bottom: Snippet;
+	let snippet_top: Snippet = $state(null);
+	let snippet_bottom: Snippet = $state(null);
 </script>
 
 <FormAddEdit bind:top={snippet_top} bind:bottom={snippet_bottom} {params} {show} close={() => (show = false)} />
-<Modal title={params.id === null ? 'Add a new account' : 'Edit account'} bind:show bind:top={snippet_top} bind:bottom={snippet_bottom} />
+<Modal title={params.id === null ? 'Add a new account' : 'Edit account'} bind:show top={snippet_top} bottom={snippet_bottom} />
