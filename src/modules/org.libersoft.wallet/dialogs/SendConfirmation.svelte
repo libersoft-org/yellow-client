b95b6760c301067ea99822cb0692187ed0043f1f
Your Name <koom@c1>
2025-08-25 18:29:40 +0200

    POL POL

--- src/modules/org.libersoft.wallet/dialogs/SendConfirmation.svelte
+++ src/modules/org.libersoft.wallet/dialogs/SendConfirmation.svelte
@@ -81,7 +81,7 @@
 				<span class="bold">{formatBalance(amountBalance)}</span>
 			{/if}
 		</div>
-		<div>Transaction fee: <span class="bold">{formatBalance({ amount: params.fee, currency: $selectedNetwork?.currency.symbol || '', decimals: 18 })} {$selectedNetwork?.currency.symbol || ''}</span></div>
+		<div>Transaction fee: <span class="bold">{formatBalance({ amount: params.fee, currency: $selectedNetwork?.currency.symbol || '', decimals: 18 })}</span></div>
 		<div>
 			Estimated time:
 			{#if $transactionTimeLoading}
