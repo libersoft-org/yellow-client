c1391a4840e09ffae4a81e2729f69807360f015a
LiberSoft <info@libersoft.org>
2025-08-30 17:18:21 +0200

    Add loading spinner to photo cards when fetching data

--- src/modules/org.libersoft.dating/components/PhotoCards.svelte
+++ src/modules/org.libersoft.dating/components/PhotoCards.svelte
@@ -2,29 +2,46 @@
 	import { module } from '@/org.libersoft.dating/scripts/module.ts';
 	import { type IPhotoCard } from '@/org.libersoft.dating/scripts/photocards.ts';
 	import PhotoCard from './PhotoCard.svelte';
+	import Spinner from '@/core/components/Spinner/Spinner.svelte';
 
-	let photos: IPhotoCard[] = $state([
-		{
-			img: 'modules/' + module.identifier + '/img/photos/1.webp',
-			name: 'Name 1',
-			description: 'Description 1',
-		},
-		{
-			img: 'modules/' + module.identifier + '/img/photos/2.webp',
-			name: 'Name 2',
-			description: 'Description 2',
-		},
-		{
-			img: 'modules/' + module.identifier + '/img/photos/3.webp',
-			name: 'Name 3',
-			description: 'Description 3',
-		},
-	]);
+	function generateMockCards(): IPhotoCard[] {
+		return [
+			{
+				img: 'modules/' + module.identifier + '/img/photos/1.webp',
+				name: 'Name 1',
+				description: 'Description 1',
+			},
+			{
+				img: 'modules/' + module.identifier + '/img/photos/2.webp',
+				name: 'Name 2',
+				description: 'Description 2',
+			},
+			{
+				img: 'modules/' + module.identifier + '/img/photos/3.webp',
+				name: 'Name 3',
+				description: 'Description 3',
+			},
+		];
+	}
+
+	let photos: IPhotoCard[] = $state(generateMockCards());
+	let isLoading = $state(false);
 
 	function removeTopCard() {
 		if (photos.length > 0) {
 			photos = photos.slice(1);
+			// If cards run out, load new ones
+			if (photos.length === 0) reloadCards();
 		}
+	}
+
+	async function reloadCards() {
+		isLoading = true;
+		// Simulation of data loading (in real app this would be an API call)
+		await new Promise(resolve => setTimeout(resolve, 1000));
+		// Load new cards
+		photos = generateMockCards();
+		isLoading = false;
 	}
 </script>
 
@@ -51,38 +68,33 @@
 		left: 0;
 		width: 100%;
 		height: 100%;
-		transform-origin: center bottom;
-		transition: transform 0.3s ease-out;
 	}
 
-	.card-wrapper:nth-child(1) {
-		z-index: 3;
-		transform: scale(1) translateY(0px);
-	}
-
-	.card-wrapper:nth-child(2) {
-		z-index: 2;
-		transform: scale(0.95) translateY(10px);
-		filter: brightness(0.9);
-	}
-
-	.card-wrapper:nth-child(3) {
-		z-index: 1;
-		transform: scale(0.9) translateY(20px);
-		filter: brightness(0.8);
-	}
-
-	.card-wrapper:nth-child(n + 4) {
-		display: none;
+	.spinner-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		height: 100%;
 	}
 </style>
 
 <div class="photo-cards-container">
 	<div class="card-stack">
-		{#each photos as photo, index (photo.name + index)}
-			<div class="card-wrapper">
-				<PhotoCard {photo} onCardRemoved={removeTopCard} />
+		{#if isLoading}
+			<div class="spinner-container">
+				<Spinner size="40px" />
 			</div>
-		{/each}
+		{:else if photos.length > 0}
+			{#each photos as photo, index (photo.name + index)}
+				<div class="card-wrapper" style="z-index: {photos.length - index}">
+					<PhotoCard {photo} onCardRemoved={removeTopCard} />
+				</div>
+			{/each}
+		{:else}
+			<div class="spinner-container">
+				<p>No more cards to display</p>
+			</div>
+		{/if}
 	</div>
 </div>
