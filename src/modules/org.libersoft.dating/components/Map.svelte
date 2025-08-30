c1d13599059028f7bdbd1b7d5870ed02d6a9fbfb
LiberSoft <info@libersoft.org>
2025-08-30 21:06:16 +0200

    Refactor Map component to use internal coordinates.

--- src/modules/org.libersoft.dating/components/Map.svelte
+++ src/modules/org.libersoft.dating/components/Map.svelte
@@ -2,18 +2,29 @@
 	import { onMount } from 'svelte';
 	import L from 'leaflet';
 	import 'leaflet/dist/leaflet.css';
-	interface Props {
+	import Spinner from '@/core/components/Spinner/Spinner.svelte';
+	interface ICoordinates {
 		latitude: string;
 		longitude: string;
 	}
-	let { latitude, longitude }: Props = $props();
-	let map;
+	let myCoordinates: ICoordinates = $state({
+		latitude: '50.0755',
+		longitude: '14.4378',
+	});
+	let map: any;
 	let mapContainer = $state();
 
 	onMount(() => {
-		map = L.map(mapContainer).setView([latitude, longitude], 13);
-		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-		L.marker([latitude, longitude]).addTo(map).bindPopup('Random place').openPopup();
+		if (mapContainer && myCoordinates.latitude && myCoordinates.longitude) {
+			map = L.map(mapContainer).setView([parseFloat(myCoordinates.latitude), parseFloat(myCoordinates.longitude)], 13);
+			L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+				attribution: '© OpenStreetMap contributors',
+			}).addTo(map);
+			L.marker([parseFloat(myCoordinates.latitude), parseFloat(myCoordinates.longitude)])
+				.addTo(map)
+				.bindPopup('Your location')
+				.openPopup();
+		}
 	});
 </script>
 
@@ -22,8 +33,20 @@
 		height: 100%;
 		width: 100%;
 	}
+
+	.loading {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+		width: 100%;
+	}
 </style>
 
-{#if latitude && longitude}
+{#if myCoordinates.latitude && myCoordinates.longitude}
 	<div class="map" bind:this={mapContainer}></div>
+{:else}
+	<div class="loading">
+		<Spinner size="100px" />
+	</div>
 {/if}
