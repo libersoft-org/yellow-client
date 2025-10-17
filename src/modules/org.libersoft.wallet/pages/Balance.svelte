357b5d902e6384aedcd6812b9af0e8f2d04f2ffc
Your Name <koom@c1>
2025-10-17 15:33:30 +0200

    cleanup

--- src/modules/org.libersoft.wallet/pages/Balance.svelte
+++ src/modules/org.libersoft.wallet/pages/Balance.svelte
@@ -12,7 +12,6 @@
 	import BalanceNfts from '@/org.libersoft.wallet/components/BalanceNfts.svelte';
 	import { stringifyWithBigInt } from '@/core/scripts/utils/utils.js';
 	import { section } from '@/org.libersoft.wallet/scripts/ui';
-	import Spinner from '@/core/components/Spinner/Spinner.svelte';
 
 	onMount(() => {
 		return initializeRefreshSystem();
