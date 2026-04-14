f25414079f550858eac0b7c6db052103386a53d5
LiberSoft <info@libersoft.org>
2026-04-15 00:46:02 +0200

    Remove effect from Intersector

--- src/modules/org.libersoft.messages/components/Intersector/Intersector.svelte
+++ src/modules/org.libersoft.messages/components/Intersector/Intersector.svelte
@@ -1,22 +1,18 @@
 <script lang="ts">
-	import { onMount, onDestroy } from 'svelte';
+	import { onDestroy } from 'svelte';
 	let { items, item_slot } = $props();
-	let observer: IntersectionObserver | undefined;
 	let visibility = $state({});
-
-	onMount(() => {
-		observer = new IntersectionObserver(intersecting, { threshold: 0.01 });
-	});
+	let observer = new IntersectionObserver(intersecting, { threshold: 0.01 });
 
 	onDestroy(() => {
-		observer?.disconnect();
+		observer.disconnect();
 	});
 
 	function observeItem(node: HTMLElement) {
-		observer?.observe(node);
+		observer.observe(node);
 		return {
 			destroy() {
-				observer?.unobserve(node);
+				observer.unobserve(node);
 			},
 		};
 	}
@@ -34,15 +30,8 @@
 
 {#each items as item, i (item.id)}
 	<div class="item" data-id={item.id} use:observeItem>
-		<!--
-  {JSON.stringify(typeof item.id)}
-  {JSON.stringify(item.id)}
-  {JSON.stringify(visibility[item.id])}
-   -->
 		<div style="min-height: {200}px;">
-			<!--{#if visibility[item.id]}-->
 			{@render item_slot(item, visibility[item.id] ? true : false)}
-			<!--{/if}-->
 		</div>
 	</div>
 {/each}
